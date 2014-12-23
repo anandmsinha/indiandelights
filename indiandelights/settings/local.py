d5792fad4750d0b361332e555602c8da3ef0ac40
Anand <anandms91@gmail.com>
2014-12-23 20:50:36 +0530

    try again

--- indiandelights/settings/local.py
+++ indiandelights/settings/local.py
@@ -11,7 +11,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'static'),
